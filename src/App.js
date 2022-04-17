b3020dd42c800e9a328d7edbff4af5d16de2f19b
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-17 12:18:21 +0600

    Not Found Page Added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from "./Pages/Homepage/HomePage";
 import Login from "./Pages/Login/Login";
 import RequireAuth from "./Pages/Login/RequireAuth/RequireAuth";
 import SignUp from "./Pages/Login/SignUp";
+import NotFound from "./Pages/NotFound/NotFound";
 import Header from "./Pages/Shared/Header/Header";
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
             </RequireAuth>
           }
         ></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import notfound from "../../Images/404/notFound.jpg";
+const NotFound = () => {
+  return (
+    <div>
+      <img className="w-100" src={notfound} alt="" />
+    </div>
+  );
+};
+
+export default NotFound;
